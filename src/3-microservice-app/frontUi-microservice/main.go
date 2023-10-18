70e36f33a33395021113cddab3ec9b3fdecc8999
Monishparameswaran <monishparameshwaran@gmail.com>
2023-10-19 00:46:17 +0530

    updated dockerfiles

--- src/3-microservice-app/frontUi-microservice/main.go
+++ src/3-microservice-app/frontUi-microservice/main.go
@@ -12,12 +12,12 @@ func main() {
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		// Redirect to the Hello microservice running on localhost:1002
-		http.Redirect(w, r, "http://helloui:8082", http.StatusSeeOther)
+		http.Redirect(w, r, "helloui:8082", http.StatusSeeOther) 
 	})
 
 	http.HandleFunc("/hostinfo", func(w http.ResponseWriter, r *http.Request) {
 		// Redirect to the HostInfo microservice running on localhost:1003
-		http.Redirect(w, r, "http://hostinfo:8084", http.StatusSeeOther)
+		http.Redirect(w, r, "hostinfo:8084", http.StatusSeeOther)
 	})
 	log.Println("Started serving at http://localhost:8080")
 	log.Println("Please make sure all containers are in custom bridge network in case of Docker");
